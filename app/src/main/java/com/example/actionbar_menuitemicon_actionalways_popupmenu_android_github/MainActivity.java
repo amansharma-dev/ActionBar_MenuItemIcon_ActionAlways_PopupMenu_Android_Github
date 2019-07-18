d7f7fcbda9bb197a826720249fbebca007835b90
Aman Sharma <amansharma.11@hotmail.com>
2019-07-19 04:59:13 +0530

    commit

--- app/src/main/java/com/example/actionbar_menuitemicon_actionalways_popupmenu_android_github/MainActivity.java
+++ app/src/main/java/com/example/actionbar_menuitemicon_actionalways_popupmenu_android_github/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.actionbar_menuitemicon_actionalways_popupmenu_android_github;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,64 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        if(id == R.id.action_menu){
+            Intent intent = new Intent(this,MenuActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void popupMenuButton(View view){
+
+        PopupMenu popupMenu = new PopupMenu(this,view);
+        popupMenu.getMenuInflater().inflate(R.menu.popup_menu,popupMenu.getMenu());
+
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                int id = item.getItemId();
+                switch (id){
+                    case R.id.action_menuOne:
+                        Toast.makeText(getApplicationContext(), "Menu 1 Clicked", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.action_menuTwo:
+                        Toast.makeText(getApplicationContext(), "Menu 2 Clicked", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.action_menuThree:
+                        Toast.makeText(getApplicationContext(), "Menu 3 Clicked", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.action_menuFour:
+                        Toast.makeText(getApplicationContext(), "Menu 4 Clicked", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.action_menuFive:
+                        Toast.makeText(getApplicationContext(), "Menu 5 Clicked", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+
+                return false;
+            }
+        });
+
+        popupMenu.show();
     }
 }
